6337a4f08fcd8918c1810f4d53697840279cc1e5
truong_faker <134347167+vantruong1812@users.noreply.github.com>
2023-06-06 10:08:02 +0700

    Delete hangso.cpp

--- hangso.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-#define PI 3.1415
-int main() {
-	// dung "const + kieu bien + ten bien"  de tao ra hang so hoac su dung khai bao "#define + ten bien + so"
-const double  PI = 3.1415; //dung cai nay thi ko dc dung #define nua 
-cout<<PI;
-return 0;
-}
