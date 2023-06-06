0d0ad73584b7749b163913789df29bb3abf9fd39
truong_faker <134347167+vantruong1812@users.noreply.github.com>
2023-06-06 08:50:06 +0700

    Add files via upload

---
+++ ontapchuong2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+	
+int main() {
+
+
+int a,b;
+cin>>a>>b;
+int tong = a+b;
+int hieu = abs(a-b);
+cout<<(tong - hieu)/2;
+
+	return 0;
+
+}
+
